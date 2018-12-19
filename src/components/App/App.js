2551dd99317526e2c76227640d073da758044b9a
Caroline Bartholdi <carolinebartholdi@Carolines-MacBook-Air.local>
2018-12-19 22:15:15 +0100

    Now runnable

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import ConnectedComposeTest from '../../containers/ConnectedComposeTest/ConnectedComposeTest';
-
 const App = () => {
   return (
     <div className='app-container' data-test-id='app-container'>
-      <ConnectedComposeTest />
+      HEJ
     </div>
   );
 };
